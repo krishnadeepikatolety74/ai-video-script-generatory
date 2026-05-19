b081491d30d8e4ddbc5d66639ebca6c097456920
Krishna Deepika <krishnadeepika9704@gmail.com>
2026-05-20 01:03:15 +0530

    fix render deployment

--- app.py
+++ app.py
@@ -225,4 +225,6 @@ def test_ai():
     return jsonify({'hook': hook})  
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port=int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
